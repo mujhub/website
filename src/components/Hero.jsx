35098c2ae44d3076d439889e23c7a5933fa9bbca
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-03-06 00:08:59 +0530

    Refactoring

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,45 +1,44 @@
 import React from "react";
-import { H3, Type, Caption } from "../styles/Shared/StyledTypes";
+
+import { H4, Type, Caption } from "../styles/Shared/StyledTypes";
 import { Hero as HeroDiv } from "../styles/components/HomeStyles";
 import { CallToActionButton } from "../styles/Shared/Button";
-import { FaGooglePlay } from "react-icons/fa";
+
+// icons
 import { FiDownload } from "react-icons/fi";
+
 export const Hero = () => {
   return (
     <>
       <HeroDiv>
         <div className="desktop">
-          <H3 style={{ margin: "1rem 0" }}>MUJ HUB</H3>
+          <H4 style={{ margin: "1rem 0 1.5rem 0" }}>MUJ HUB</H4>
           <Type>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo,
             morbi aenean diam nulla. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Justo, morbi aenean diam nulla. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Justo, morbi aenean diam
-            nulla.
+            adipiscing elit.
           </Type>
 
           <Type>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo,
             morbi aenean diam nulla. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Justo, morbi aenean diam nulla. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Justo, morbi aenean diam
-            nulla
+            adipiscing elit.
           </Type>
         </div>
 
         <div className="mobile">
           <div className="buttons">
-            <CallToActionButton Primary>
+            {/* <CallToActionButton Primary>
               <FaGooglePlay style={{ marginRight: "0.5rem" }} /> Get it on
               Google Play
-            </CallToActionButton>
-            <CallToActionButton Outline>
+            </CallToActionButton> */}
+            <CallToActionButton Primary>
               <FiDownload style={{ marginRight: "0.5rem" }} /> Install The
               Application
             </CallToActionButton>
           </div>
 
-          <Caption>Coming soon to iOS</Caption>
+          <Caption>Coming soon to Android and iOS</Caption>
         </div>
       </HeroDiv>
     </>
