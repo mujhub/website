ce81781c22bb39b8e66866eb24ca93067c1b2b6c
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-03-06 01:10:46 +0530

    added Auth context

---
+++ src/contexts/Auth.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState, createContext, useContext } from "react";
+import { auth } from "../services/firestore";
+
+export const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+	const [currentUser, setCurrentUser] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+
+	const login = (email, password) => {
+		return auth.signInWithEmailAndPassword(email, password);
+	};
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user);
+			setIsLoading(false);
+		});
+		return unsubscribe;
+	}, []);
+
+	const value = { currentUser, login };
+
+	return <AuthContext.Provider value={value}>{!isLoading && children}</AuthContext.Provider>;
+};
