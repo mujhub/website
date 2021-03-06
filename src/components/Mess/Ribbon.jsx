82a22093e445b3eb376114d7cf1f8435958c97a2
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-03-07 02:57:24 +0530

    [CSS] Mess Menu auth alerts/ribbons

--- src/components/Mess/Ribbon.jsx
+++ src/components/Mess/Ribbon.jsx
@@ -2,19 +2,25 @@ import React from "react";
 import { useAuth } from "../../contexts/Auth";
 import { auth } from "../../services/firestore";
 
+// styles
+import { RibbonStyle } from "../../styles/components/MessStyles";
+import { Type } from "../../styles/Shared/StyledTypes";
+
 const Ribbon = () => {
-	const { currentUser } = useAuth();
+  const { currentUser } = useAuth();
 
-	const handleLogout = () => {
-		auth.signOut();
-	};
+  const handleLogout = () => {
+    auth.signOut();
+  };
 
-	return (
-		<div>
-			{"Hello, " + currentUser.email}
-			<div onClick={handleLogout}>Logout</div>
-		</div>
-	);
+  return (
+    <RibbonStyle>
+      {"Hello, " + currentUser.email}
+      <Type onClick={handleLogout}>
+        <u>LOGOUT</u>
+      </Type>
+    </RibbonStyle>
+  );
 };
 
 export default Ribbon;
