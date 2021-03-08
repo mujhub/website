565249928be2fd56a394273c31815dd3e86411cb
manavendrasen <manavendra.199302058@muj.manipal.edu>
2021-03-09 00:15:12 +0530

    [+] Team Page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Container } from "../styles/Shared/Container";
 import { H4, Type } from "../styles/Shared/StyledTypes";
-import { Subheading } from "../styles/components/HomeStyles";
-
+import { TeamGrid, TeamHeaderContainer } from "../styles/components/TeamStyles";
 // team card
 import { TeamCard } from "../components/Team/TeamCard";
 
@@ -10,22 +9,33 @@ import { TeamCard } from "../components/Team/TeamCard";
 import team from "../constants/teamMembers";
 
 const Team = () => {
-	return (
-		<>
-			<Subheading>
-				<H4>Our Team</H4>
-				<Type>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nesciunt possimus sapiente amet similique quo alias dignissimos dolor provident.
-				</Type>
-			</Subheading>
+  return (
+    <>
+      <TeamHeaderContainer>
+        <H4>Our Team</H4>
+        <Type className="TeamHeaderDesc">
+          Team MUJ HUB works super hard to make your life at MUJ easier. If you
+          happen to like our work and see us on campus, don't forget to give us
+          a high five xD
+        </Type>
+      </TeamHeaderContainer>
 
-			<Container center>
-				{team.map((member) => {
-					return <TeamCard name={member.name} post={member.post} links={member.links} />;
-				})}
-			</Container>
-		</>
-	);
+      <Container center>
+        <TeamGrid>
+          {team.map((member) => {
+            return (
+              <TeamCard
+                img={member.img}
+                name={member.name}
+                post={member.post}
+                links={member.links}
+              />
+            );
+          })}
+        </TeamGrid>
+      </Container>
+    </>
+  );
 };
 
 export default Team;
