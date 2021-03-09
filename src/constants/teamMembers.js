bbfbdc113645aa7e55cdb6d3f62fabfa70ea0857
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-03-09 16:44:55 +0530

    compressed images

--- src/constants/teamMembers.js
+++ src/constants/teamMembers.js
@@ -1,105 +1,105 @@
 // images
-import vaibhav from "../images/TeamImages/vaibhav.jpeg";
+import vaibhav from "../images/TeamImages/vaibhav.jpg";
 import gourav from "../images/TeamImages/gourav.jpg";
-import siddhesh from "../images/TeamImages/siddhesh.jpeg";
-import divyanshu from "../images/TeamImages/divyanshu.jpeg";
+import siddhesh from "../images/TeamImages/siddhesh.jpg";
+import divyanshu from "../images/TeamImages/divyanshu.jpg";
 import manavendra from "../images/TeamImages/manavendra.jpg";
 import kartik from "../images/TeamImages/kartik.jpg";
 const team = [
-  {
-    name: "Vaibhav Garg",
-    post: "Full Stack Developer",
-    img: vaibhav,
-    links: [
-      {
-        href: "https://www.linkedin.com/in/vaibhavgarg24",
-        icon: "Linkedin",
-      },
-      {
-        href: "https://github.com/21vaibhavgarg",
-        icon: "Github",
-      },
-    ],
-  },
-  {
-    name: "Gourav Moonka",
-    post: "PR & DB Manager",
-    img: gourav,
-    links: [
-      {
-        href: "https://github.com/heyy-gm",
-        icon: "Linkedin",
-      },
-      {
-        href: "https://instagram.com/heyy_gm",
-        icon: "Instagram",
-      },
-    ],
-  },
-  {
-    name: "Siddhesh Kataria",
-    post: "Marketing Head",
-    img: siddhesh,
-    links: [
-      {
-        href: "https://www.linkedin.com/in/siddheshkataria/",
-        icon: "Linkedin",
-      },
-      {
-        href: "https://www.instagram.com/sidd_kataria/",
-        icon: "Instagram",
-      },
-    ],
-  },
-  {
-    name: "Divyanshu Sharma",
-    post: "Backend Developer",
-    img: divyanshu,
-    links: [
-      {
-        href: "https://www.linkedin.com/in/d1vshar",
-        icon: "Linkedin",
-      },
-      {
-        href: "https://github.com/d1vshar",
-        icon: "Github",
-      },
-    ],
-  },
-  {
-    name: "Manavendra Sen",
-    post: "UI/UX Designer & Web Developer",
-    img: manavendra,
-    links: [
-      {
-        href: "https://www.linkedin.com/in/manavendrasen/",
-        icon: "Linkedin",
-      },
-      {
-        href: "https://github.com/manavendrasen",
-        icon: "Github",
-      },
-      {
-        href: "https://www.behance.net/manavendrasen",
-        icon: "Behance",
-      },
-    ],
-  },
-  {
-    name: "Kartik Goel",
-    post: "Full Stack Developer",
-    img: kartik,
-    links: [
-      {
-        href: "https://www.linkedin.com/in/kg-kartik/",
-        icon: "Linkedin",
-      },
-      {
-        href: "https://github.com/kg-kartik",
-        icon: "Github",
-      },
-    ],
-  },
+	{
+		name: "Vaibhav Garg",
+		post: "Full Stack Developer",
+		img: vaibhav,
+		links: [
+			{
+				href: "https://www.linkedin.com/in/vaibhavgarg24",
+				icon: "Linkedin",
+			},
+			{
+				href: "https://github.com/21vaibhavgarg",
+				icon: "Github",
+			},
+		],
+	},
+	{
+		name: "Gourav Moonka",
+		post: "PR & DB Manager",
+		img: gourav,
+		links: [
+			{
+				href: "https://github.com/heyy-gm",
+				icon: "Linkedin",
+			},
+			{
+				href: "https://instagram.com/heyy_gm",
+				icon: "Instagram",
+			},
+		],
+	},
+	{
+		name: "Siddhesh Kataria",
+		post: "Marketing Head",
+		img: siddhesh,
+		links: [
+			{
+				href: "https://www.linkedin.com/in/siddheshkataria/",
+				icon: "Linkedin",
+			},
+			{
+				href: "https://www.instagram.com/sidd_kataria/",
+				icon: "Instagram",
+			},
+		],
+	},
+	{
+		name: "Divyanshu Sharma",
+		post: "Backend Developer",
+		img: divyanshu,
+		links: [
+			{
+				href: "https://www.linkedin.com/in/d1vshar",
+				icon: "Linkedin",
+			},
+			{
+				href: "https://github.com/d1vshar",
+				icon: "Github",
+			},
+		],
+	},
+	{
+		name: "Manavendra Sen",
+		post: "UI/UX Designer & Web Developer",
+		img: manavendra,
+		links: [
+			{
+				href: "https://www.linkedin.com/in/manavendrasen/",
+				icon: "Linkedin",
+			},
+			{
+				href: "https://github.com/manavendrasen",
+				icon: "Github",
+			},
+			{
+				href: "https://www.behance.net/manavendrasen",
+				icon: "Behance",
+			},
+		],
+	},
+	{
+		name: "Kartik Goel",
+		post: "Full Stack Developer",
+		img: kartik,
+		links: [
+			{
+				href: "https://www.linkedin.com/in/kg-kartik/",
+				icon: "Linkedin",
+			},
+			{
+				href: "https://github.com/kg-kartik",
+				icon: "Github",
+			},
+		],
+	},
 ];
 
 export default team;
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { Container } from "../styles/Shared/Container";
 import { H4, Type } from "../styles/Shared/StyledTypes";
-import {
-  TeamGrid,
-  TeamHeaderContainer,
-  TeamContainer,
-} from "../styles/components/TeamStyles";
+import { TeamGrid, TeamHeaderContainer, TeamContainer } from "../styles/components/TeamStyles";
 // team card
 import { TeamCard } from "../components/Team/TeamCard";
 
@@ -13,33 +9,26 @@ import { TeamCard } from "../components/Team/TeamCard";
 import team from "../constants/teamMembers";
 
 const Team = () => {
-  return (
-    <TeamContainer>
-      <TeamHeaderContainer>
-        <H4>Our Team</H4>
-        <Type className="TeamHeaderDesc">
-          Team MUJ HUB works super hard to make your life at MUJ easier. If you
-          happen to like our work and see us on campus, don't forget to give us
-          a high five xD
-        </Type>
-      </TeamHeaderContainer>
+	return (
+		<TeamContainer>
+			<TeamHeaderContainer>
+				<H4>Our Team</H4>
+				<Type className='TeamHeaderDesc'>If you happen to like our work and see us on campus, don't forget to give us a high five xD</Type>
+			</TeamHeaderContainer>
 
-      <Container center>
-        <TeamGrid>
-          {team.map((member) => {
-            return (
-              <TeamCard
-                img={member.img}
-                name={member.name}
-                post={member.post}
-                links={member.links}
-              />
-            );
-          })}
-        </TeamGrid>
-      </Container>
-    </TeamContainer>
-  );
+			<Container center>
+				<TeamGrid>
+					{team.map((member, i) => {
+						return (
+							<div key={i.toString()}>
+								<TeamCard img={member.img} name={member.name} post={member.post} links={member.links} />
+							</div>
+						);
+					})}
+				</TeamGrid>
+			</Container>
+		</TeamContainer>
+	);
 };
 
 export default Team;
