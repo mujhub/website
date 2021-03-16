af0488ec37fd2521b7f2012fc75c1fbcd0d19942
21VAibhavGArg <myselfvaibhavgarg@gmail.com>
2021-03-16 13:12:38 +0530

    fixed service worker

--- public/worker.js
+++ public/worker.js
@@ -1,12 +1,18 @@
-var CACHE_NAME = "pwa-task-manager";
+// IMPORTANT! Update this cache name and cache whitelist on subsequent builds
+
+var CURRENT_VERSION = "29d0444c"; //current cache name
+var CACHE_VERSION_WHITELIST = ["29d0444c"]; //names of current and previous caches which can be used further
+
 var urlsToCache = ["/"];
 
 // Install a service worker
 self.addEventListener("install", (event) => {
+	//skip the waiting phase
+	self.skipWaiting();
+
 	// Perform install steps
 	event.waitUntil(
-		caches.open(CACHE_NAME).then(function (cache) {
-			console.log("Opened cache");
+		caches.open(CURRENT_VERSION).then(function (cache) {
 			return cache.addAll(urlsToCache);
 		})
 	);
@@ -16,7 +22,6 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
 	event.respondWith(
 		caches.match(event.request).then(function (response) {
-			// Cache hit - return response
 			if (response) {
 				return response;
 			}
@@ -27,12 +32,11 @@ self.addEventListener("fetch", (event) => {
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
-	var cacheWhitelist = ["pwa-task-manager"];
 	event.waitUntil(
 		caches.keys().then((cacheNames) => {
 			return Promise.all(
 				cacheNames.map((cacheName) => {
-					if (cacheWhitelist.indexOf(cacheName) === -1) {
+					if (CACHE_VERSION_WHITELIST.indexOf(cacheName) === -1) {
 						return caches.delete(cacheName);
 					}
 				})
